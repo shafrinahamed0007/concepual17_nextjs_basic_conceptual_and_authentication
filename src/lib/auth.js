fddbba837f49b01a57ef22db54834c9914eacbe7
shafrinahamed0007 <mrdeveloperdevv@gmail.com>
2026-05-04 05:45:29 +0600

    configure better auth and mongodb connect with site

--- src/lib/auth.js
+++ src/lib/auth.js
@@ -1,5 +1,16 @@
 import { betterAuth } from "better-auth";
+import { MongoClient } from "mongodb";
+import { mongodbAdapter } from "better-auth/adapters/mongodb";
+
+const client = new MongoClient(process.env.MONGODB_URI);
+const db = client.db("better-auth-3");
 
 export const auth = betterAuth({
+  database: mongodbAdapter(db, {
+    client,
+  }),
+  emailAndPassword: {
+    enabled: true,
+  },
   //...
 });
